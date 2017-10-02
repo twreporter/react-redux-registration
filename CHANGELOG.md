c5df996d0e924734829db30614941e8450dfb470
Han <allen0346@gmail.com>
2017-10-02 13:41:37 +0800

    [Update] remove next from package.json for twreporter-react

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+### 1.0.0-beta.0.0.6
+
+- Remove next from package.json
+- Modified sing-in-next.js for twreporter-react (Errors caused by removing next)
+
 ### 1.0.0-beta.0.0.3
 
 - Initialization
--- src/containers/sign-in-next.js
+++ src/containers/sign-in-next.js
@@ -1,34 +1,35 @@
-import Link from 'next/link'
+// import Link from 'next/link'
 import React from 'react'
-import Router from 'next/router'
-import SignInPrototype from './sign-in-prototype'
-import styled from 'styled-components'
-import { SIGN_UP } from '../constants/string'
+// import Router from 'next/router'
+// import SignInPrototype from './sign-in-prototype'
+// import styled from 'styled-components'
+// import { SIGN_UP } from '../constants/string'
 
-const LinkStyle = styled.a`
-  color: #0366D6;
-  opacity: 0.7;
-  &:hover {
-    cursor: pointer;
-  }
-`
+// const LinkStyle = styled.a`
+//   color: #0366D6;
+//   opacity: 0.7;
+//   &:hover {
+//     cursor: pointer;
+//   }
+// `
+//
+// const signUpArea = (() => {
+//   return (
+//     <div>
+//       <span>{`${SIGN_UP.plainText}`}&nbsp;</span>
+//       <Link href="/signup"><LinkStyle>{`${SIGN_UP.linkText}`}</LinkStyle></Link>
+//     </div>
+//   )
+// })()
 
-const signUpArea = (() => {
+const SignInReact = () => {
   return (
-    <div>
-      <span>{`${SIGN_UP.plainText}`}&nbsp;</span>
-      <Link href="/signup"><LinkStyle>{`${SIGN_UP.linkText}`}</LinkStyle></Link>
-    </div>
-  )
-})()
-
-const SignInReact = (props) => {
-  return (
-    <SignInPrototype
-      signUpArea={signUpArea}
-      router={Router}
-      {...props}
-    />
+    // <SignInPrototype
+    //   signUpArea={signUpArea}
+    //   router={Router}
+    //   {...props}
+    // />
+    <span />
   )
 }
 
